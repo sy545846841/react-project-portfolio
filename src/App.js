c2400b2967fcf300451b83a6d67874de6d3439f0
sy546846841 <44683670+sy545846841@users.noreply.github.com>
2021-10-19 12:49:15 -0700

    6. Style Services Section

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
 import ServiceSection from "./components/ServiceSection";
 import FaqSection from "./components/FaqSection";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <AboutUs />
       <ServiceSection />
       <FaqSection />
