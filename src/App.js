9c1f4defe755112bd4ac58fb5a6a173d75df1c6e
sy546846841 <44683670+sy545846841@users.noreply.github.com>
2021-10-19 00:00:50 -0700

    4. FAQ Section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import react from "react";
 import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
 import ServiceSection from "./components/ServiceSection";
+import FaqSection from "./components/FaqSection";
+
 function App() {
   return (
     <>
       <AboutUs />
       <ServiceSection />
+      <FaqSection />
     </>
   );
 }
