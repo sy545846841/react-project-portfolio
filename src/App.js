a604bf32480c50d51efea3a3dc7ab6ce2eaa6b55
sy546846841 <44683670+sy545846841@users.noreply.github.com>
2021-10-19 15:49:50 -0700

    9.Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import react from "react";
-import AboutSection from "./components/AboutSection";
-import AboutUs from "./pages/AboutUs";
-import ServiceSection from "./components/ServiceSection";
-import FaqSection from "./components/FaqSection";
+//Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Import Pages
+import Nav from "./components/Nav";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <AboutUs />
-      <ServiceSection />
-      <FaqSection />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </>
   );
 }
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,15 @@
 import React from "react";
 //page components
 import AboutSection from "../components/AboutSection";
+import ServiceSection from "../components/ServiceSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
+      <ServiceSection />
+      <FaqSection />
     </div>
   );
 };
